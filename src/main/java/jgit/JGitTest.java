66210e864b066aea4a04064b02e7f73fad704e49
cloudqht <1286020832@qq.com>
2023-01-06 15:30:51 +0800

    1

--- src/main/java/jgit/JGitTest.java
+++ src/main/java/jgit/JGitTest.java
@@ -14,7 +14,7 @@ public class JGitTest {
     static String password = "";
     static String privateToken = "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";
     static String remoteName = "origin";
-    static String branchName = "dev";
+    static String branchName = "master";
     public static void main(String[] args) {
 //        System.out.println(JGitUtils.openRpo(localPath));
 //        System.out.println(JGitUtils.openNowRpo());
@@ -23,11 +23,11 @@ public class JGitTest {
 //        Git git = JGitUtils.openRpo(localPath);
         Git git = JGitUtils.openNowRpo();
         String filepattern = ".";
-        JGitUtils.add(git,filepattern);
-        JGitUtils.commit(git, "second commit");
+//        JGitUtils.add(git,filepattern);
+//        JGitUtils.commit(git, "second commit");
 //        JGitUtils.printLog(git);
 //        JGitUtils.push(git, username, password, remoteName, branchName, privateToken);
-//        JGitUtils.pull(git, username, password, branchName, privateToken);
+        JGitUtils.pull(git, username, password, branchName, privateToken);
 //        JGitUtils.branchCreate(git, "dev");
 //        JGitUtils.branchCheckout(git, "dev");
 //        JGitUtils.branchDelete(git, "dev");
